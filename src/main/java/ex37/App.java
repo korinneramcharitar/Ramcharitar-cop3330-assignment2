6ad06feb44d2fc0b3afec811466d63c81d7c3462
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-13 22:24:33 -0400

    Update App.java

--- src/main/java/ex37/App.java
+++ src/main/java/ex37/App.java
@@ -1,11 +1,17 @@
 package ex37;
-
+import java.util.Random;
 import java.util.Scanner;
 public class App {
-    public static void main(int n) {
+    public static void main(String[] args) {
         int length;
         int chars;
         int num;
+        String cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String low = "abcdefghijklmnopqrstuvwxyz";
+        String special = "!@#$%^&*()";
+        String numbers = "1234567890";
+        String combination = cap + low +  special  + numbers;
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Whats the minimum length?");
@@ -17,6 +23,19 @@ public class App {
 
         System.out.println("How many numbers?");
         num= in.nextInt();
-
+        
+        String result = "";
+        String fin = "";
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(combination.length());
+            result +=  combination.charAt(index);
+        }
+        System.out.println("Your password is: " + result);
     }
-}
+    }
+
+
+
+
+
